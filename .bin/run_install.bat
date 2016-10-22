a13ca6ac6c94652219333b47c4cbd8c0b2d3f6f8
mixa marciv <mixamarciv@gmail.com>
2016-10-22 19:08:23 +0300

    поправили немножко мелочи

--- .bin/run_install.bat
+++ .bin/run_install.bat
@@ -12,6 +12,9 @@ go get "github.com/martini-contrib/binding"
 go get "github.com/martini-contrib/sessions"
 go get "github.com/codegangsta/martini-contrib/render"
 go get "github.com/mixamarciv/gofncstd3000"
+go get "github.com/go-gomail/gomail"
+go get "github.com/nakagami/firebirdsql"
+go get "github.com/nfnt/resize"
 
 go install
 
